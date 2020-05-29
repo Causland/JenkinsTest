136c4a30083a01c7d82d236e94b35fa1941df90a
Causland <christopher.auslander@gmail.com>
2020-05-29 09:09:21 -0400

    rearrange structure

---
+++ test/ShapesTest.cpp
@@ -0,0 +1,7 @@
+#include "test/gtest.h"
+#include "Rectangle.h"
+
+TEST (NameTest, Rectangle){
+    Rectangle rec(10,2);
+    EXPECT_EQ("Rectangle of size a=20.000000, p=24.000000", rec.toString());
+}
